f6e9a2dfb73b722eef92865f06a068c7dba86dab
ext.zhanglong5 <ext.zhanglong5@jd.com>
2023-12-15 16:23:12 +0800

    fix: 图片加载修改

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,25 @@ module.exports = defineConfig({
     client: {
       overlay: false,
     },
-  }
+  },
+  // proxy: {
+  //   [`/api/cwapi/`]: {
+  //     target: 'http://test-gxf.jd.com',
+  //     ws: false,
+  //     pathRewrite: {
+  //       [`/api/cwapi/`]: '' // 需要rewrite的,
+  //     },
+  //     changeOrigin: true,
+  //     logLevel: 'debug'
+  //   },
+  //   [`${process.env.VUE_APP_API_BASE_PATH}/`]: {
+  //     target: require('minimist')(process.argv.slice(3))?.requestUrl || process.env.VUE_APP_API_BASE_URL,
+  //     ws: false,
+  //     changeOrigin: true,
+  //     // pathRewrite: {
+  //     //   [`^${process.env.VUE_APP_API_BASE_PATH}`]: '' // 需要rewrite的,
+  //     // },
+  //     logLevel: 'debug'
+  //   }
+  // }
 })
